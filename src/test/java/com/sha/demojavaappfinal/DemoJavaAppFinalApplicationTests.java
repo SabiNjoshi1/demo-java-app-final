8cc317af298edd050bbf530b39989ed5d2cb2511
Sabinjoshi1 <sabinjoshi044@gmail.com>
2023-05-22 15:50:53 +0545

    all file added

---
+++ src/test/java/com/sha/demojavaappfinal/DemoJavaAppFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sha.demojavaappfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaAppFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
